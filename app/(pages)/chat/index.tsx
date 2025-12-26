59f4382d32399df3297b373dbb59d2c4e6324bb2
Brunoooo <brunotenorio15@hotmail.com>
2025-12-26 08:59:34 -0300

    Ajustes ae

--- app/(pages)/chat/index.tsx
+++ app/(pages)/chat/index.tsx
@@ -1,10 +1,10 @@
 import StyledBackground from "@/components/StyledBackground";
 import { Feather } from "@expo/vector-icons";
-import { KeyboardAvoidingView, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "@/styles/chat.styles";
 import { Colors } from "@/constants/Style.data";
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Chat() {
     const router = useRouter();
@@ -12,35 +12,59 @@ export default function Chat() {
     const [status, setStatus] = useState("Online");
 
 
+    // Estado para controlar a altura do padding inferior
+    const [paddingBottom, setPaddingBottom] = useState(0);
+
+    useEffect(() => {
+        // Definir os nomes dos eventos dependendo da plataforma
+        // iOS tem animação 'Will', Android geralmente funciona melhor com 'Did' ou 'Will' dependendo da versão
+        const showEvent = Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow';
+        const hideEvent = Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide';
+
+        const keyboardShowListener = Keyboard.addListener(showEvent, (e) => {
+            // e.endCoordinates.height pega a altura exata do teclado
+            setPaddingBottom((e.endCoordinates.height) + 20);
+        });
+
+        const keyboardHideListener = Keyboard.addListener(hideEvent, () => {
+            setPaddingBottom(0); // Reseta para 0 quando fecha
+        });
+
+        return () => {
+            keyboardShowListener.remove();
+            keyboardHideListener.remove();
+        };
+    }, []);
+
+
     return (
         <StyledBackground>
-            <KeyboardAvoidingView style={{ flex: 1 }} behavior="height" >
 
-                <View style={styles.header}>
-                    <TouchableOpacity onPress={() => router.back()}>
-                        <Feather name="arrow-left" size={24} color="black" />
-                    </TouchableOpacity>
-                    <View style={styles.avatarBox}>
-                        <View style={styles.avatar}>
-                            <Text>Avatar</Text>
-                        </View>
-                        <View style={styles.info}>
-                            <Text style={styles.textName}>Name</Text>
-                            <View style={styles.statusBox}>
-                                {
-                                    status === "Online" ?
-                                        <View style={styles.statusOptionOn}></View>
-                                        : <View style={styles.statusOptionOff}></View>
-                                }
-                                <Text style={styles.text}>{status}</Text>
-                            </View>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => router.back()}>
+                    <Feather name="arrow-left" size={24} color="black" />
+                </TouchableOpacity>
+                <View style={styles.avatarBox}>
+                    <View style={styles.avatar}>
+                        <Text>Avatar</Text>
+                    </View>
+                    <View style={styles.info}>
+                        <Text style={styles.textName}>Name</Text>
+                        <View style={styles.statusBox}>
+                            {
+                                status === "Online" ?
+                                    <View style={styles.statusOptionOn}></View>
+                                    : <View style={styles.statusOptionOff}></View>
+                            }
+                            <Text style={styles.text}>{status}</Text>
                         </View>
                     </View>
-                    <TouchableOpacity>
-                        <Feather name="more-vertical" size={24} color="black" />
-                    </TouchableOpacity>
                 </View>
-
+                <TouchableOpacity>
+                    <Feather name="more-vertical" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={{flex: 1, paddingBottom: paddingBottom }}>
                 <View style={styles.content}>
                     <ScrollView showsVerticalScrollIndicator={false}>
                         <View>
@@ -65,7 +89,8 @@ export default function Chat() {
                         </TouchableOpacity>
                     </View>
                 </View>
-            </KeyboardAvoidingView>
+            </View>
+        </StyledBackground >
+    );
 
-        </StyledBackground >);
 }
